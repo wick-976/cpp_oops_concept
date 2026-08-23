09931bc1333f3903b580c2a8bacaab1cb14c9951
wick-976 <cbtharan976@gmail.com>
2026-08-23 06:23:15 +0530

    Class and Object in cpp

---
+++ class and opbject Creation.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+class Student{
+
+    public:
+        int rollno;
+        string name;
+
+};
+int main()
+{
+
+    Student tharan;
+
+    tharan.rollno=622;
+    tharan.name="Tharan";
+
+    cout<<tharan.name;
+    cout<<tharan.rollno;
+}
