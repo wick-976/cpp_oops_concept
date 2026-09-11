8be9c0bbfc9e6d7327fd69cc881747fd482d62c7
wick-976 <cbtharan976@gmail.com>
2026-09-11 06:17:44 +0530

    Upto inserting before head

---
+++ Linked List/Inserting_a_node.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* back;
+
+    Node(int val) {
+        this->val = val;
+        this->next = NULL;
+        this->back = NULL;
+    }
+
+    Node(int val, Node* next, Node* back) {
+        this->val = val;
+        this->next = next;
+        this->back = back;
+    }
+};
+
+Node* link_list_create(vector<int> arr) {
+    if (arr.empty()) return NULL;
+
+    Node* hd = new Node(arr[0]);
+    Node* prev = hd;
+
+    for (int i = 1; i < arr.size(); i++) {
+        Node* nn = new Node(arr[i], NULL, prev);
+        prev->next = nn;
+        prev = nn;
+    }
+
+    return hd;
+}
+
+void print(Node* hd) {
+    Node* temp = hd;
+    while (temp != NULL) {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node* Delete_node(Node* hd, Node* del) {
+    if (del == NULL) return hd;
+
+    // If deleting head
+    if (del == hd) {
+        Node* newHead = hd->next;
+        if (newHead != NULL) newHead->back = NULL;
+        delete hd;
+        return newHead;
+    }
+
+    Node* prev = del->back;
+    Node* front = del->next;
+
+    if (prev != NULL) prev->next = front;
+    if (front != NULL) front->back = prev;
+
+    delete del;
+    return hd;
+}
+void insertion_before_head (Node* hd,int value)
+{
+    Node* nn=new Node(7,hd,NULL);
+
+    hd->back=nn;
+
+    print(nn);
+
+}
+
+int main() {
+    vector<int> num = {1, 2, 3, 4, 5};
+    Node* head = link_list_create(num);
+
+    print(head);
+
+    // Delete the 3rd node (value 3)
+    head = Delete_node(head, head->next->next);
+
+    print(head);
+
+    cout<<"Enter the value you need to insert = ";
+    int value;
+    cin>>value;
+    insertion_before_head(head,value);
+
+    return 0;
+}
